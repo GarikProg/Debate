121bae356d9216bfa0030d1c3101b6c91c31fad0
Igor <kozlovigor1989@gmail.com>
2020-10-15 19:53:26 +0300

    Release ws 0

--- back-end/index.js
+++ back-end/index.js
@@ -1,67 +1,67 @@
-import express from 'express';
-import session from 'express-session';
-import './misc/env.js';
-import './misc/db.js';
-import authRouter from './routes/auth.js';
-import userMiddleware from './middlewares/user.js';
-import notFoundMiddleware from './middlewares/notfound.js';
-import errorMiddleware from './middlewares/error.js';
-import MongoDB from 'connect-mongodb-session';
-import ws from 'ws'
+import express from "express";
+import session from "express-session";
+import "./misc/env.js";
+import "./misc/db.js";
+import authRouter from "./routes/auth.js";
+import userMiddleware from "./middlewares/user.js";
+import notFoundMiddleware from "./middlewares/notfound.js";
+import errorMiddleware from "./middlewares/error.js";
+import MongoDB from "connect-mongodb-session";
+import ws from "ws";
+import cors from "cors";
+import ioSocket from "socket.io";
+const io = ioSocket();
 
 const logger = console;
 const app = express();
 const MongoDBStore = MongoDB(session);
 const store = new MongoDBStore({
   uri: process.env.DB_URL,
-  collection: 'sessions'
+  collection: "sessions",
 });
 
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT ?? 3001;
 const httpServer = app.listen(port, () => {
-  logger.log('Сервер запущен. Порт:', port);
+  logger.log("Сервер запущен. Порт:", port);
 });
 
-const wsServer = new ws.Server({
-  server: httpServer,
+io.on("connection", (socket) => {
+  console.log("connection open");
+  socket.broadcast.emit("broadcast", "hello from socket")
+  client.on("message", (data) => {
+    console.log(data);
+    io.broadcast.emit("broadcast", data);
+  });
 });
 
-wsServer.on('connection', (client) => {
-  client.on('message', (message) => {
-  console.log('>>>>message', message);
-  const obj = JSON.parse(message);
-  wsServer.clients.forEach((client) => {
-    clien.send(newMessage);
-  })  
-}
-)
-})
 
-
+io.listen(process.env.PORT_SOCKET);
+console.log("listening on port ", process.env.PORT_SOCKET);
 
 // Запоминаем название куки для сессий
-app.set('session cookie name', 'sid');
+app.set("session cookie name", "sid");
 
-app.set('trust proxy', 1);
+app.set("trust proxy", 1);
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(session({
-  name: app.get('session cookie name'),
-  secret: process.env.SESSION_SECRET,
-  store: store,
-  // Если true, сохраняет сессию, даже если она не поменялась
-  resave: false,
-  // Если false, куки появляются только при установке req.session
-  saveUninitialized: true,
-  cookie: {
-    // В продакшне нужно "secure: true" для HTTPS
-    secure: process.env.NODE_ENV === 'production',
-  },
-}));
+app.use(
+  session({
+    name: app.get("session cookie name"),
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    // Если true, сохраняет сессию, даже если она не поменялась
+    resave: false,
+    // Если false, куки появляются только при установке req.session
+    saveUninitialized: true,
+    cookie: {
+      // В продакшне нужно "secure: true" для HTTPS
+      secure: process.env.NODE_ENV === "production",
+    },
+  })
+);
 app.use(userMiddleware);
 app.use(authRouter);
 
-
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
-
