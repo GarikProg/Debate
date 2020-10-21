16d1ba0acbdeee61dc7784e685cfb97fa2682309
Ivan.A <hleb247@gmail.com>
2020-10-21 12:49:42 +0300

    ivan fixes

--- front-end/src/components/Header/header.js
+++ front-end/src/components/Header/header.js
@@ -63,9 +63,9 @@ const Header = ({ history }) => {
             </div>
             <div className="menu">
               <button className="profileStyle" disabled={disabled} onClick={handleMenu}>
-              <Link className="profileLinkStyle">
+              <div className="profileLinkStyle">
                 Menu
-                </Link>
+                </div>
               </button>
               {isAuthorized ? <button className="profileStyle"><Link className="profileLinkStyle" to="/createThread">Create Thread </Link></button> : <button className="profileStyle"><Link className="profileLinkStyle" to="/Auth">Authentication </Link></button>}
               <button className="profileStyle">
