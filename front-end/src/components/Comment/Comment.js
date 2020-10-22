654598b54029cc3f7ea86dc26c8274040e95e44c
Ivan.A <hleb247@gmail.com>
2020-10-22 12:09:16 +0300

    ivan added colors

--- front-end/src/components/Comment/Comment.js
+++ front-end/src/components/Comment/Comment.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import "./comment.scss"
 
@@ -18,9 +18,17 @@ export default function Comment(props) {
     challenge,
   } = props;
   
-  
+  const comment = () => {
+    const colorArr = ['one', 'two', 'three','four','five','six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen']
+    return colorArr[(Math.floor(Math.random() * 13))];
+  }
+  useEffect(() => {    
+    console.log(Math.floor(Math.random() * 13) + 1);
+    console.log(comment());
+  }, []);
+
   return (
-    <div className="comment">
+    <div className={comment()}>
       <div><span>_{nickName}_</span></div>
       <span>{side}_</span>
       <span>{text}_</span>
