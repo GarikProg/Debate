09d0218c52079d4bd1c88dd66fc86884e560ece0
Ivan.A <hleb247@gmail.com>
2020-10-23 15:47:54 +0300

    ivan front fixes

--- front-end/src/components/GlobalThread/globalThread.js
+++ front-end/src/components/GlobalThread/globalThread.js
@@ -3,16 +3,46 @@ import openSocket from "socket.io-client";
 import { useParams, Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import Comment from "../Comment/Comment";
-import { createNewDebate, addLikeToUserInRedux, addCommentToUserInRedux, changeCommetWritingPermission, setCommetWritingCooldown, addCommentCountToCommentsInRedux } from '../../redux/actions'
-import './globalthread.scss'
+import {
+  createNewDebate,
+  addLikeToUserInRedux,
+  addCommentToUserInRedux,
+  changeCommetWritingPermission,
+  setCommetWritingCooldown,
+  addCommentCountToCommentsInRedux,
+} from "../../redux/actions";
+import "./globalthread.scss";
 
 const comment = () => {
-  const colorArr = ['one', 'two', 'three','four','five','six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen','fourteen', 'fifteen','sixteen','seventeen','eighteen','nineteen','twenty','twentyone','twentytwo','twentythree']
-  return colorArr[(Math.floor(Math.random() * 23))];
+  const colorArr = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+    "twenty",
+    "twentyone",
+    "twentytwo",
+    "twentythree",
+  ];
+  return colorArr[Math.floor(Math.random() * 23)];
 };
 
 function GlobalThread() {
-
   const { id } = useParams();
   const dispatch = useDispatch();
 
@@ -21,43 +51,42 @@ function GlobalThread() {
   const [outPut, setOutput] = useState([]);
   const [side, setSide] = useState("Neutral");
   const [thread, setThread] = useState({});
-  const [colors, setColors] = useState(["red", "red", "red"])
+  const [colors, setColors] = useState(["red", "red", "red"]);
 
   useEffect(() => {
-    setColors(state => ([comment(), comment(), comment()]))
-  }, [])
+    setColors((state) => [comment(), comment(), comment()]);
+  }, []);
 
-  
+  const nickName = useSelector((state) => state.user.name);
 
-  const nickName = useSelector((state) => state.user.name);  
-  
   const creator = useSelector((state) => state.user._id);
-  
-  const isAuthorized = useSelector(state => state.isAuthorized);
-  
+
+  const isAuthorized = useSelector((state) => state.isAuthorized);
+
   // Логика кулдауна
-  const coolDown = useSelector(state => state.commentWritingTimeout);
-  const canWriteComment = useSelector(state => state.canWriteComment);
+  const coolDown = useSelector((state) => state.commentWritingTimeout);
+  const canWriteComment = useSelector((state) => state.canWriteComment);
 
   // Конвертер времени для отображения
   function convertNumberToTime(num) {
     let seconds = num % 60;
     let minutes = Math.floor(num / 60);
     return `${minutes}:${seconds}`;
-  };
+  }
 
   // Подгружаем конкретный тред из редакса
-  const appThreads = useSelector(state => state.appThreads)
+  const appThreads = useSelector((state) => state.appThreads);
   useEffect(() => {
-    appThreads && appThreads.filter(el => {
-      if (el._id == id) {
-        setThread(el);
-        setOutput(el.comments)
-      }
-    });
+    appThreads &&
+      appThreads.filter((el) => {
+        if (el._id == id) {
+          setThread(el);
+          setOutput(el.comments);
+        }
+      });
   }, [appThreads]);
 
-  useEffect(() => {    
+  useEffect(() => {
     const socket = openSocket("http://localhost:8000", {
       query: {
         id,
@@ -73,8 +102,8 @@ function GlobalThread() {
         if (data.commentLocation) {
           // Присылает класс Comment
           console.log(data);
-          dispatch(addCommentToUserInRedux(data))
-          dispatch(addCommentCountToCommentsInRedux(id, data))
+          dispatch(addCommentToUserInRedux(data));
+          dispatch(addCommentCountToCommentsInRedux(id, data));
           setOutput((prev) => {
             return [...prev, data];
           });
@@ -82,18 +111,18 @@ function GlobalThread() {
         if (data.comment) {
           // Присылает класс Like
           dispatch(addLikeToUserInRedux(data));
-          setOutput((prev) =>                    
+          setOutput((prev) =>
             prev.map((el, i) => {
-              if (el._id === data.comment) {                
+              if (el._id === data.comment) {
                 return {
                   ...el,
-                   likes: [...el.likes, data],
+                  likes: [...el.likes, data],
                 };
               }
               return el;
             })
           );
-         }        
+        }
       });
   }, [socket]);
 
@@ -103,81 +132,121 @@ function GlobalThread() {
     dispatch(changeCommetWritingPermission());
     dispatch(setCommetWritingCooldown(60));
 
-    socket.send({ type: "comment", text, id, side, nickName, creator, from: "thread" });
+    socket.send({
+      type: "comment",
+      text,
+      id,
+      side,
+      nickName,
+      creator,
+      from: "thread",
+    });
   };
 
-
   const punch = useCallback((comment_id, socket, creator) => {
-      // Отправка лайка на бек
-      socket.send({ type: "like", comment_id, creator, id });
-  }, [])
-  
+    // Отправка лайка на бек
+    socket.send({ type: "like", comment_id, creator, id });
+  }, []);
+
   const challenge = useCallback((comment_creator) => {
-    console.log(creator, 'ghggh', comment_creator);
-    dispatch(createNewDebate( { creator, participant: comment_creator}))
-  },[]);
+    console.log(creator, "ghggh", comment_creator);
+    dispatch(createNewDebate({ creator, participant: comment_creator }));
+  }, []);
 
   return (
     <>
-    <div className="gobalest">
-      <div className="headers">
-        {/* <div>
+      <div className="gobalest">
+        <div className="headers">
+          {/* <div>
             <div>Your nickname: </div>
             <div className={`${colors[2]} nickClass`}>{nickName}</div>
         </div> */}
-        <div>
+          <div>
             <div>Theme: </div>
             <div className={colors[0]}>{thread.theme}</div>
+          </div>
+          <div>
+            <div>Description: </div>
+            <div className={colors[1]}>{thread.description}</div>
+          </div>
         </div>
-        <div>
-          <div>Description: </div>
-          <div className={colors[1]}>{thread.description}</div>
-        </div>
-      </div>
-      {/* END OF HEADERS */}
-      <div className="inputGlob">
-          <button className="sideButton"  onClick={() => setSide(thread.sideOne)}>
+        {/* END OF HEADERS */}
+        <div className="inputGlob">
+          <button
+            className="sideButton"
+            onClick={() => setSide(thread.sideOne)}
+          >
             {thread.sideOne}
           </button>
-          <button className="sideButton"  onClick={() => setSide(thread.sideTwo)}>
+          <button
+            className="sideButton"
+            onClick={() => setSide(thread.sideTwo)}
+          >
             {thread.sideTwo}
           </button>
-    {isAuthorized ?
-    <>
-        <form className="inputForm" id="messageForm" onSubmit={ (e) => handleSubmit(e) }>
-          {canWriteComment ? <><button className="sideButton sideButtonTwo" type="submit">Punch</button> <input placeholder="   TYPE HERE" className="inputGlobInner"
-            onBlur={(e) => {
-              e.preventDefault();
-              setText(e.target.value)
-            }}
-            type="text"
-            name="message"
-            id="message"
-          /> </>: <div className="sideButtonCD">Следующий комментарий можно написать через { convertNumberToTime(coolDown) }</div>}
-          
-        </form>
-    </> : <> <Link to="/Auth"><button>Sign in to punch and vote</button></Link> </>}      
-      </div>  
-    
-      <div className="chat">
-      {outPut &&
-        outPut.map((el, index) => {
-          return (
-            <Comment
-              key={ el._id }
-              index={ index }
-              comment_id={ el._id }
-              text={ el.text }
-              side={ el.side }
-              likes={ el.likes }
-              punch={ punch }
-              challenge={ challenge }
-              creator={ el.creator }
-              socket={ socket }
-            />
-          );
-        })}
-      </div>
+          {isAuthorized ? (
+            <>
+              <form
+                className="inputForm"
+                id="messageForm"
+                onSubmit={(e) => handleSubmit(e)}
+              >
+                {canWriteComment ? (
+                  <>
+                    <button className="sideButton sideButtonTwo" type="submit">
+                      Punch
+                    </button>{" "}
+                    <input
+                      placeholder="   TYPE HERE"
+                      className="inputGlobInner"
+                      onBlur={(e) => {
+                        e.preventDefault();
+                        setText(e.target.value);
+                      }}
+                      type="text"
+                      name="message"
+                      id="message"
+                    />{" "}
+                  </>
+                ) : (
+                  <div className="sideButtonCD">
+                    Следующий комментарий можно написать через{" "}
+                    {convertNumberToTime(coolDown)}
+                  </div>
+                )}
+              </form>
+            </>
+          ) : (
+            <>
+              {" "}
+              <Link to="/Auth">
+                <button className="sideButton">
+                  Sign in to punch and vote
+                </button>
+              </Link>{" "}
+            </>
+          )}
+        </div>
+
+        <div className="chat">
+          {outPut &&
+            outPut.map((el, index) => {
+              return (
+                <Comment
+                  key={el._id}
+                  index={index}
+                  comment_id={el._id}
+                  text={el.text}
+                  side={el.side}
+                  likes={el.likes}
+                  punch={punch}
+                  challenge={challenge}
+                  creator={el.creator}
+                  socket={socket}
+                />
+              );
+            })}
+        </div>
       </div>
     </>
   );
--- front-end/src/components/GlobalThreadAll/GlobalThreadAll.js
+++ front-end/src/components/GlobalThreadAll/GlobalThreadAll.js
@@ -1,33 +1,40 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useSelector } from 'react-redux';
-import './globalthreadAll.scss'
+import { useSelector } from "react-redux";
+import "./globalthreadAll.scss";
 
 export default function GlobalThreadAll() {
-  
-  const isAuthorized = useSelector(state => state.isAuthorized);
-  
-  const threads = useSelector(state => state.appThreads);
-  
+  const isAuthorized = useSelector((state) => state.isAuthorized);
+
+  const threads = useSelector((state) => state.appThreads);
+
   return (
     <div className="globalAll">
       {threads &&
         threads.map((el) => {
           return (
-          <div className="thread">
-            <Link className="linkGlobal" to={`/GlobalThread/${el._id}`}>
-              <button className="theme" >{el.theme}</button>
-            </Link>
-            <span className="question"> ? </span>
-            <span className="sideOne">{el.sideOne}</span>
-            <span className="colon"> : </span>
-            <span className="sideTwo">{el.sideTwo}</span>
-          </div>);
-        })}    
+            <div className="thread">
+                <span className="threadInner"><Link className="linkGlobal" to={`/GlobalThread/${el._id}`}>
+                  <button className="theme">{el.theme}</button>
+                </Link></span>
+
+              <span className="spanInner">
+                <span className="question"> ? </span>
+                <span className="sideOne">{el.sideOne}</span>
+                <span className="colon"> : </span>
+                <span className="sideTwo">{el.sideTwo}</span>
+              </span>
+            </div>
+          );
+        })}
       <div>
-        <br/>
-        {isAuthorized && <Link to="/createThread"><button className="createButton">Create Thread</button></Link>}  
-      </div>    
+        <br />
+        {isAuthorized && (
+          <Link to="/createThread">
+            <button className="sideButton">Create Thread</button>
+          </Link>
+        )}
+      </div>
     </div>
   );
 }
